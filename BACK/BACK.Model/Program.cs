13a2f885106c777319bf14a4d9708e6a8684b46c
rbrito <rbrito@IMPWS00396>
2022-01-16 16:23:16 -0300

    criado arquivo de projeto model

---
+++ BACK/BACK.Model/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BACK.Model
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
