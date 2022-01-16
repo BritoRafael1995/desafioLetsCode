93d2d45c2045ca55833bf58828ac18674e3d158e
rbrito <rbrito@IMPWS00396>
2022-01-16 17:40:06 -0300

    criadas interfaces e implementação da business card

---
+++ BACK/BACK.Business/Implementation/CardBusiness.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BACK.Business.Implementation
+{
+    public class CardBusiness : ICardBusiness
+    {
+    }
+}
---
+++ BACK/BACK.Business/Implementation/ICardBusiness.cs
@@ -0,0 +1,6 @@
+﻿namespace BACK.Business.Implementation
+{
+    internal interface ICardBusiness
+    {
+    }
+}
