8d0565073b9f1c7c21920b6474a03c525af7ed48
rbrito <rbrito@IMPWS00396>
2022-01-16 16:23:39 -0300

    criados modelos card e user

---
+++ BACK/BACK.Model/Models/Card.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BACK.Model.Models
+{
+    public class Card
+    {
+        public Guid Id { get; set; }
+        public string Titulo { get; set; }
+        public string Conteudo { get; set; }
+        public string Lista { get; set; }
+    }
+}
