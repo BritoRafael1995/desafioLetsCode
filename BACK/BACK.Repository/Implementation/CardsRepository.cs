d8be8714bc4199ac01086fbea9603dfb25301663
rbrito <rbrito@IMPWS00396>
2022-01-16 17:46:45 -0300

    criada interface e implementação do cardsRepository

---
+++ BACK/BACK.Repository/Implementation/CardsRepository.cs
@@ -0,0 +1,11 @@
+﻿using BACK.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BACK.Repository.Implementation
+{
+    public class CardsRepository : ICardsRepository
+    {
+    }
+}
---
+++ BACK/BACK.Repository/Interface/ICardsRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BACK.Repository.Interface
+{
+    public interface ICardsRepository
+    {
+    }
+}
